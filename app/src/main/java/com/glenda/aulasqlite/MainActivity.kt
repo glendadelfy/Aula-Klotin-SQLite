52d705b3180a3b14d9e8ab2fa6296d0e08a7c039
glenda <glendadelfy20@gmail.com>
2024-09-27 12:29:29 -0300

    Aula de SQLite Local

---
+++ app/src/main/java/com/glenda/aulasqlite/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.glenda.aulasqlite
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.glenda.aulasqlite.bancodedados.DataBaseHelper
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    private val bancoDados by lazy {
+        DataBaseHelper(this)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        try {
+            val sql = "INSERT INTO produtos VALUES(null, 'Celular Samsung S24+', '128gb')"
+            bancoDados.writableDatabase.execSQL(sql)
+            Log.i("db_info", "Registro Salvo")
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+    }
+}
---
+++ app/src/main/java/com/glenda/aulasqlite/bancodedados/DataBaseHelper.kt
@@ -0,0 +1,38 @@
+package com.glenda.aulasqlite.bancodedados
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import java.lang.Exception
+
+class DataBaseHelper(context:Context):SQLiteOpenHelper(
+    //1.Context
+    //2.Nome do banco de dados
+    //3.CursoFactory
+    //4.Versão do banco
+
+    context,"loja",null,1
+
+) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        //É executado uma unica vez, quando o app é instalado
+        val sql = "CREATE TABLE IF NOT EXISTS produtos(\n" +
+                "  id_produto INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+                "  titulo VARCHAR(100),\n" +
+                "  descricao TEXT\n" +
+                "  )"
+        try {
+            db?.execSQL(sql)
+            Log.i("db_info", "Tabela criada com sucesso")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.i("db_info", "Error ao criar tabela")
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
